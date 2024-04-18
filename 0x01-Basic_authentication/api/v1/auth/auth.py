c06ccf0d98d21deb4e041ac39e77e7136066caf1
loaywaleed <loaywaleed9@gmail.com>
2024-04-18 23:18:02 +0200

    excluded routes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,14 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth public method handling"""
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[-1] != '/':
+            path[-1] += '/'
+
+        if path in excluded_paths:
+            return False
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
