1ca93fede887796a5b3338c3c2af2b798aa376bd
loaywaleed <loaywaleed9@gmail.com>
2024-04-18 23:06:58 +0200

    status code hotfix

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -30,7 +30,7 @@ def unauthorized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """forbidden error handler"""
-    return jsonify({"error": "Forbidden"})
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
