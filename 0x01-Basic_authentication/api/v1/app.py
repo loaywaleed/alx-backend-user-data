5881328af27cd606559012bd774c08581e2f42ca
loaywaleed <loaywaleed9@gmail.com>
2024-04-18 22:58:55 +0200

    doc change

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -23,8 +23,7 @@ def not_found(error) -> str:
 
 @app.errorhandler(401)
 def unauthorized(error) -> str:
-    """ Unauthorized handler
-    """
+    """Unauthorized error handler"""
     return jsonify({"error": "Unauthorized"}), 401
 
 
