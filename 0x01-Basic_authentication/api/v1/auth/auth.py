95b6d0c2649ab933b553791e47fb7e370a20e561
loaywaleed <loaywaleed9@gmail.com>
2024-04-21 20:32:34 +0200

    authorization header

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,7 +21,9 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """Authorization header"""
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar("User"):
         """Current logged in user"""
