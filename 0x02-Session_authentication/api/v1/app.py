d63879492f27de6d482b3a8479ba73d45d27a66a
loaywaleed <loaywaleed9@gmail.com>
2024-04-22 19:18:59 +0200

    current_user

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -34,8 +34,12 @@ def before_request():
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+    current_user = auth.current_user(request)
+
+    if current_user is None:
         abort(403)
+
+    request.current_user = current_user
 
 
 @app.errorhandler(404)
