bd21cabbe8617714b4735e2bf6f56399e378f5d6
loaywaleed <loaywaleed9@gmail.com>
2024-04-22 09:09:49 +0200

    hotfix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,9 +9,10 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Class that uses basic auth for authentication"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
 
-        if authorization_header is Nonee or authorization_header != type(str):
+        if authorization_header is None or authorization_header != type(str):
             return None
         if not authorization_header.startswith("Basic "):
             return None
