3ca2c1647e6f22bc0b07afc8f1afe062853a08d3
loaywaleed <loaywaleed9@gmail.com>
2024-04-22 09:37:29 +0200

    decoding base64 value

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Basic Authorization module
 """
 
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,17 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Decoding the Base64 value in the header"""
+        if self.extract_base64_authorization_header(
+                base64_authorization_header) is None:
+            return None
+        try:
+            value = base64_authorization_header.encode('utf-8')
+            decoded = b64decode(value)
+            decoded_value = decoded.encode('utf-8')
+        except BaseException:
+            return None
+        return decoded_value
