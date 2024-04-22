c2602dae79cdb48cf8339a1fdd6d8ab3e2c5856b
loaywaleed <loaywaleed9@gmail.com>
2024-04-22 09:27:33 +0200

    considering the two first parts

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -18,4 +18,4 @@ class BasicAuth(Auth):
             return None
         if not authorization_header.startswith("Basic "):
             return None
-        return authorization_header.split(" ")[1:]
+        return authorization_header.split(" ", 1)[1]
