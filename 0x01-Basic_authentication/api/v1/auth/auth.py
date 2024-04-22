85ef239a6cfed8fb125aded408bd42fcd10dd931
loaywaleed <loaywaleed9@gmail.com>
2024-04-22 12:05:24 +0200

    testing * support

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,6 +17,15 @@ class Auth:
         if path[-1] != "/":
             path += "/"
 
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*"):
+                prefix = excluded_path[:-1]
+                if path.startswith(prefix):
+                    return False
+
         return path not in excluded_paths
 
     def authorization_header(self, request=None) -> str:
