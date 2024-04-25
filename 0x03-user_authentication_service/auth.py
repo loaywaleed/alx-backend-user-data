313b0095b85fd01584a4550d7ca68d52a7718fd1
loaywaleed <loaywaleed9@gmail.com>
2024-04-25 21:49:27 +0200

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,6 +5,7 @@ Authorization module
 
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """Hashing password and returning it"""
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
