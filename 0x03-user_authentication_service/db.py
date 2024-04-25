7c5786d14f86c0bb4c32e2d3bbb106424407deeb
loaywaleed <loaywaleed9@gmail.com>
2024-04-25 10:52:45 +0200

    finding user by filter

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import Base, User
 
 
@@ -37,3 +39,16 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+    
+    def find_user_by(self, **kwargs) -> User:
+        """Filteration method
+        """
+        accepted_keys = ['email', 'id', 'hashed_password', 'session_id',
+                          'reset_token']
+        for key in kwargs.keys():
+            if key not in accepted_keys:
+                raise InvalidRequestError
+        value = self._session.query().filter_by(**kwargs).first()
+        if value is None:
+            raise NoResultFound
+        return value
