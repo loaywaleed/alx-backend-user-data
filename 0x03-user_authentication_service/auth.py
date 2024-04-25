37c90adab57621cf6cde880a074e273454bec35e
loaywaleed <loaywaleed9@gmail.com>
2024-04-25 21:48:39 +0200

    Hashing passwords

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Authorization module
+"""
+
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashing password and returning it"""
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
