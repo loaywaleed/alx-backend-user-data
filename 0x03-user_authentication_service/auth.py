407fd4f7a6d3f388cc06cdc338eb04951f8a0054
loaywaleed <loaywaleed9@gmail.com>
2024-04-26 05:25:11 +0300

    reset password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -67,3 +67,13 @@ class Auth:
     def destroy_session(self, user_id: int) -> None:
         """destorys a user's session"""
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Updates user token and returns it"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        reset_token = str(uuid4())
+        self._db.update_user(user.id, reset_token=reset_token)
+        return reset_token
