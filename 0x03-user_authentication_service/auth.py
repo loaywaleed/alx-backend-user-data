3a5d761aa096db806d4db21a9c612be6a5575cb5
loaywaleed <loaywaleed9@gmail.com>
2024-04-26 05:07:05 +0300

    pycodestyle

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,7 +63,7 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except NoResultFound:
             return None
-        
+
     def destroy_session(self, user_id: int) -> None:
         """destorys a user's session"""
         self._db.update_user(user_id, session_id=None)
