f83f0fc08c50af5c2937b211f0d969999e7ade95
loaywaleed <loaywaleed9@gmail.com>
2024-04-26 05:45:45 +0300

    testing update password edge case

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -93,7 +93,7 @@ def update_password() -> str:
     new_password = request.form.get("new_password")
     try:
         AUTH.update_password(reset_token, new_password)
-    except ValueError:
+    except Exception:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"}), 200
 
